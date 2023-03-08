b0e9b54a0fb6765663241bb19e2b04613c27873e
tanveersalim <tanveersalim24@gmail.com>
2023-03-08 13:24:22 -0700

    change

--- tests/Search.spec.js
+++ tests/Search.spec.js
@@ -2,49 +2,68 @@ const{test,expect}=require("@playwright/test")
 
 test.skip("Search word1",async function({page}){
 await page.goto("https://testing.ui.plentyz.pk/")
-await page.locator("//select[@name='categoryId']").selectOption("Beauty & Grooming")
-await page.locator("//input[@placeholder='Search Your Product Here']").fill("Cream")
-await page.locator("//button[@name='searchButton']").click()
+const category=page.locator("//select[@name='categoryId']")
+const searchproduct=page.locator("//input[@placeholder='Search Your Product Here']")
+const searchbtn=page.locator("//button[@name='searchButton']")
+await category.selectOption("Beauty & Grooming")
+await searchproduct.fill("Cream")
+await searchbtn.click()
 await page.waitForTimeout(5000)
 
 })
 
 test.skip("Search word2",async function({page}){
-    await page.goto("https://testing.ui.plentyz.pk/")
-    await page.locator("//select[@name='categoryId']").selectOption("Home & Lifestyle")
-    await page.locator("//input[@placeholder='Search Your Product Here']").fill("Microwave")
-    await page.locator("//button[@name='searchButton']").click()
-    await page.waitForTimeout(5000)
+
+await page.goto("https://testing.ui.plentyz.pk/")
+const category=page.locator("//select[@name='categoryId']")
+const searchproduct=page.locator("//input[@placeholder='Search Your Product Here']")
+const searchbtn=page.locator("//button[@name='searchButton']")
+await category.selectOption("Home & Lifestyle")
+await searchproduct.fill("Microwave")
+await searchbtn.click()
+await page.waitForTimeout(5000)
     
 })
 
 test.skip("Search word3",async function({page}){
-        await page.goto("https://testing.ui.plentyz.pk/")
-        await page.locator("//select[@name='categoryId']").selectOption("Mobile & Tablets")
-        await page.locator("//input[@placeholder='Search Your Product Here']").fill("Cable")
-        await page.locator("//button[@name='searchButton']").click()
-        await page.waitForTimeout(5000)
+       
+await page.goto("https://testing.ui.plentyz.pk/")
+const category=page.locator("//select[@name='categoryId']")
+const searchproduct=page.locator("//input[@placeholder='Search Your Product Here']")
+const searchbtn=page.locator("//button[@name='searchButton']")
+await category.selectOption("Mobile & Tablets")
+await searchproduct.fill("Cable")
+await searchbtn.click()
+await page.waitForTimeout(5000)
         
 })
 
 test.skip("Search word4",async function({page}){
-            await page.goto("https://testing.ui.plentyz.pk/")
-            await page.locator("//select[@name='categoryId']").selectOption("Computing")
-            await page.locator("//input[@placeholder='Search Your Product Here']").fill("Computer")
-            await page.locator("//button[@name='searchButton']").click()
-            await page.waitForTimeout(5000)
+
+await page.goto("https://testing.ui.plentyz.pk/")
+const category=page.locator("//select[@name='categoryId']")
+const searchproduct=page.locator("//input[@placeholder='Search Your Product Here']")
+const searchbtn=page.locator("//button[@name='searchButton']")
+await category.selectOption("Computing")
+await searchproduct.fill("Computer")
+await searchbtn.click()
+await page.waitForTimeout(5000)
             
 })
             
 test.skip("Search word5",async function({page}){
-                await page.goto("https://testing.ui.plentyz.pk/")
-                await page.locator("//select[@name='categoryId']").selectOption("Mobile & Tablets")
-                await page.locator("//input[@placeholder='Search Your Product Here']").fill("Power Bank")
-                await page.locator("//button[@name='searchButton']").click()
-                await page.waitForTimeout(5000)
-                await page.locator("//input[@placeholder='Search Your Product Here']").clear()
-                await page.locator("//input[@placeholder='Search Your Product Here']").fill("Cable")
-                await page.locator("//button[@name='searchButton']").click()
-                await page.waitForTimeout(5000)
+
+await page.goto("https://testing.ui.plentyz.pk/")
+const category=page.locator("//select[@name='categoryId']")
+const searchproduct=page.locator("//input[@placeholder='Search Your Product Here']")
+const searchbtn=page.locator("//button[@name='searchButton']")
+await category.selectOption("Mobile & Tablets")
+await searchproduct.fill("Power Bank")
+await searchbtn.click()
+await page.waitForTimeout(5000)
+await searchproduct.clear()
+await searchproduct.fill("Cable")
+await searchbtn.click()
+await page.waitForTimeout(5000)
 })
 
